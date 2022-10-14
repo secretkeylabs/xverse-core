a385933debf21d68da048dd546eb64a22486f20b
Mahmoud <mahmoud@checkfirstapp.com>
2022-10-14 14:18:25 +0200

    added wallet encryption and decryption utils

--- wallet/index.ts
+++ wallet/index.ts
@@ -191,3 +191,55 @@ export function validateBtcAddress({
     return false;
   }
 }
+interface EncryptMnemonicArgs {
+  password: string,
+  seed: string,
+  passwordHashGenerator: (password: string) => Promise<{
+    salt: string,
+    hash: string,
+  }>
+  mnemonicEncryptionHandler: (seed: string, key: string) => Promise<Buffer>
+}
+
+interface DecryptMnemonicArgs {
+  password: string,
+  encryptedSeed: string,
+  passwordHashGenerator: (password: string) => Promise<{
+    salt: string,
+    hash: string,
+  }>
+  mnemonicDecryptionHandler: (seed: Buffer | string, key: string) => Promise<string>
+}
+
+export async function encryptMnemonicWithCallback(cb: EncryptMnemonicArgs) {
+  const {
+    mnemonicEncryptionHandler,
+    passwordHashGenerator,
+    password,
+    seed,
+  } = cb;
+  try {
+    const { hash } = await passwordHashGenerator(password);
+    const encryptedSeedBuffer = await mnemonicEncryptionHandler(seed, hash);
+    return encryptedSeedBuffer.toString('hex');
+  } catch(err) {
+    return Promise.reject(err)
+  }
+}
+
+
+export async function decryptMnemonicWithCallback(cb: DecryptMnemonicArgs) {
+  const {
+    mnemonicDecryptionHandler,
+    passwordHashGenerator,
+    password,
+    encryptedSeed,
+  } = cb;
+  try {
+    const { hash } = await passwordHashGenerator(password);
+    const seedPhrase = await mnemonicDecryptionHandler(encryptedSeed, hash);
+    return seedPhrase;
+  } catch(err) {
+    return Promise.reject(err)
+  }
+}
