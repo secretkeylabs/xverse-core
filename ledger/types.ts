a113b7129fffdec987494af64449b8c92c9156e2
JPatrik <patrik.jokhel@dakai.io>
2023-04-06 09:59:55 +0200

    refactor: move out psbt creation

--- ledger/types.ts
+++ ledger/types.ts
@@ -1,3 +1,9 @@
-import AppClient from "ledger-bitcoin";
+import AppClient from 'ledger-bitcoin';
 
 export type Transport = ConstructorParameters<typeof AppClient>[0];
+
+export type Bip32Derivation = {
+  masterFingerprint: Buffer;
+  path: string;
+  pubkey: Buffer;
+};
