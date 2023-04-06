44f5f950b252189c084dbbc4e5d8e464af50ccfd
JPatrik <patrik.jokhel@dakai.io>
2023-04-06 09:59:55 +0200

    refactor: move out psbt creation

--- ledger/index.ts
+++ ledger/index.ts
@@ -7,9 +7,15 @@ import {
   selectUnspentOutputs,
   sumUnspentOutputs,
 } from '../transactions/btc';
-import { BtcFeeResponse, ErrorCodes, NetworkType, ResponseError } from 'types/network';
+import {
+  BtcFeeResponse,
+  BtcUtxoDataResponse,
+  ErrorCodes,
+  NetworkType,
+  ResponseError,
+} from 'types/network';
 import { getNestedSegwitAccountDataFromXpub, getPublicKeyFromXpubAtIndex } from './helper';
-import { Transport } from './types';
+import { Bip32Derivation, Transport } from './types';
 import { fetchBtcAddressUnspent } from 'api/btc';
 import { fetchBtcFeeRate } from 'api';
 import { networks, Psbt } from 'bitcoinjs-lib';
@@ -76,48 +82,20 @@ export async function importTaprootAccountFromLedger(
 }
 
 /**
- * This function is used to sign a Nested Segwit transaction with the ledger
- * @param transport - the transport object with connected ledger device
+ * This function is used to get the transaction data for the ledger psbt
  * @param network - the network type
- * @param addressIndex - the address index
- * @param recipient - the recipient of the transaction
- * @returns the signed raw transaction in hex format
+ * @param senderAddress - the sender address
+ * @param recipient - the recipient
+ * @returns the selected utxos, the change value and the fee
  * */
-
-export async function signLedgerNestedSegwitBtcTransaction(
-  transport: Transport,
+async function getTransactionData(
   network: NetworkType,
-  addressIndex: number,
+  senderAddress: string,
   recipient: Recipient
-): Promise<string> {
-  // TODO - Refactor this to sub functions
+) {
   let feeRate: BtcFeeResponse = defaultFeeRate;
-  const coinType = network === 'Mainnet' ? 0 : 1;
+  const { amountSats } = recipient;
 
-  const app = new AppClient(transport);
-
-  //================================================================================================
-  // 1. Get Account Data
-  //================================================================================================
-
-  const masterFingerPrint = await app.getMasterFingerprint();
-  const extendedPublicKey = await app.getExtendedPubkey(`m/49'/${coinType}'/0'`);
-  const accountPolicy = new DefaultWalletPolicy(
-    'sh(wpkh(@0/**))',
-    `[${masterFingerPrint}/49'/${coinType}'/0']${extendedPublicKey}`
-  );
-
-  const {
-    publicKey: senderPublicKey,
-    address: senderAddress,
-    redeemScript,
-    witnessScript,
-  } = getNestedSegwitAccountDataFromXpub(extendedPublicKey, addressIndex, network);
-  const { address: recipientAddress, amountSats } = recipient;
-
-  //================================================================================================
-  // 2. Get UTXOs
-  //================================================================================================
   const allUTXOs = await fetchBtcAddressUnspent(senderAddress, network);
   let selectedUTXOs = selectUnspentOutputs(amountSats, allUTXOs);
   let sumOfSelectedUTXOs = sumUnspentOutputs(selectedUTXOs);
@@ -138,7 +116,7 @@ export async function signLedgerNestedSegwitBtcTransaction(
     network
   );
 
-  // Recalculate the sum of selected UTXOs
+  // Recalculate the sum of selected UTXOs if new UTXOs were selected
   if (newSelectedUnspentOutputs.length !== selectedUTXOs.length) {
     selectedUTXOs = newSelectedUnspentOutputs;
     sumOfSelectedUTXOs = sumUnspentOutputs(newSelectedUnspentOutputs);
@@ -148,37 +126,56 @@ export async function signLedgerNestedSegwitBtcTransaction(
     }
   }
 
-  //================================================================================================
-  // 3. Create Transaction
-  //================================================================================================
+  const changeValue = sumOfSelectedUTXOs.minus(amountSats).minus(fee);
 
+  return { selectedUTXOs, changeValue, fee };
+}
+
+/**
+ * This function is used to create a nested segwit transaction for the ledger
+ * @param network - the network type
+ * @param recipient - the recipient
+ * @param changeAddress - the change address
+ * @param changeValue - the change value
+ * @param inputUTXOs - the selected input utxos
+ * @param inputDerivation - the derivation data for the sender address
+ * @param redeemScript - the redeem script
+ * @param witnessScript - the witness script
+ * @returns the psbt without any signatures
+ * */
+async function createNestedSegwitPsbt(
+  network: NetworkType,
+  recipient: Recipient,
+  changeAddress: string,
+  changeValue: BigNumber,
+  inputUTXOs: BtcUtxoDataResponse[],
+  inputDerivation: Bip32Derivation[] | undefined,
+  redeemScript: Buffer,
+  witnessScript: Buffer
+): Promise<Psbt> {
   const btcNetwork = network === 'Mainnet' ? networks.bitcoin : networks.testnet;
   const psbt = new Psbt({ network: btcNetwork });
+  const { address: recipientAddress, amountSats } = recipient;
 
   const transactionMap = new Map<string, Buffer>();
-  for (const utxo of newSelectedUnspentOutputs) {
+  for (const utxo of inputUTXOs) {
     const txDataApiUrl = `https://blockstream.info/testnet/api/tx/${utxo.tx_hash}/hex`;
     const response = await axios.get(txDataApiUrl);
     transactionMap.set(utxo.tx_hash, Buffer.from(response.data, 'hex'));
   }
 
-  for (const utxo of newSelectedUnspentOutputs) {
+  for (const utxo of inputUTXOs) {
     psbt.addInput({
       hash: utxo.tx_hash,
       index: utxo.tx_output_n,
       redeemScript: redeemScript,
+      // both nonWitnessUtxo and witnessUtxo are required or the ledger displays warning message
       witnessUtxo: {
         script: witnessScript,
         value: utxo.value,
       },
       nonWitnessUtxo: transactionMap.get(utxo.tx_hash),
-      bip32Derivation: [
-        {
-          path: `m/49'/${coinType}'/0'/0/${addressIndex}`,
-          pubkey: senderPublicKey,
-          masterFingerprint: Buffer.from(masterFingerPrint, 'hex'),
-        },
-      ],
+      bip32Derivation: inputDerivation,
     });
   }
 
@@ -188,10 +185,79 @@ export async function signLedgerNestedSegwitBtcTransaction(
       value: amountSats.toNumber(),
     },
     {
-      address: senderAddress,
-      value: sumOfSelectedUTXOs.minus(amountSats).minus(fee).toNumber(),
+      address: changeAddress,
+      value: changeValue.toNumber(),
     },
   ]);
+
+  return psbt;
+}
+
+/**
+ * This function is used to sign a Nested Segwit transaction with the ledger
+ * @param transport - the transport object with connected ledger device
+ * @param network - the network type
+ * @param addressIndex - the address index
+ * @param recipient - the recipient of the transaction
+ * @returns the signed raw transaction in hex format
+ * */
+
+export async function signLedgerNestedSegwitBtcTransaction(
+  transport: Transport,
+  network: NetworkType,
+  addressIndex: number,
+  recipient: Recipient
+): Promise<string> {
+  const coinType = network === 'Mainnet' ? 0 : 1;
+  const app = new AppClient(transport);
+
+  //================================================================================================
+  // 1. Get Account Data
+  //================================================================================================
+
+  const masterFingerPrint = await app.getMasterFingerprint();
+  const extendedPublicKey = await app.getExtendedPubkey(`m/49'/${coinType}'/0'`);
+  const accountPolicy = new DefaultWalletPolicy(
+    'sh(wpkh(@0/**))',
+    `[${masterFingerPrint}/49'/${coinType}'/0']${extendedPublicKey}`
+  );
+
+  const {
+    publicKey: senderPublicKey,
+    address: senderAddress,
+    redeemScript,
+    witnessScript,
+  } = getNestedSegwitAccountDataFromXpub(extendedPublicKey, addressIndex, network);
+
+  //================================================================================================
+  // 2. Get UTXOs
+  //================================================================================================
+
+  const { selectedUTXOs, changeValue } = await getTransactionData(
+    network,
+    senderAddress,
+    recipient
+  );
+
+  //================================================================================================
+  // 3. Create Transaction
+  //================================================================================================
+
+  const inputDerivation: Bip32Derivation = {
+    path: `m/49'/${coinType}'/0'/0/${addressIndex}`,
+    pubkey: senderPublicKey,
+    masterFingerprint: Buffer.from(masterFingerPrint, 'hex'),
+  };
+  const psbt = await createNestedSegwitPsbt(
+    network,
+    recipient,
+    senderAddress,
+    changeValue,
+    selectedUTXOs,
+    [inputDerivation],
+    redeemScript,
+    witnessScript
+  );
 
   //================================================================================================
   // 4. Sign Transaction
