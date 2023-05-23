a062f08934e8c8c2869f95d0f4bc6b92fbec3ff0
Victor <victor@nomanini.com>
2023-05-23 13:47:59 +0300

    feat: fallback to blockcypher api for utxos

--- api/instance.ts
+++ api/instance.ts
@@ -7,21 +7,13 @@ export default class ApiInstance {
     this.bitcoinApi = axios.create(config);
   }
 
-  async httpGet(url: string, params: any = {}): Promise<any> {
-    try {
-      const response = await this.bitcoinApi.get(url, { params });
-      return response.data;
-    } catch (e) {
-      return Promise.reject(e);
-    }
+  async httpGet<T = any>(url: string, params: any = {}): Promise<T> {
+    const response = await this.bitcoinApi.get<T>(url, { params });
+    return response.data;
   }
 
-  async httpPost(url: string, data: any): Promise<any> {
-    try {
-      const response = await this.bitcoinApi.post(url, data);
-      return response.data;
-    } catch (e) {
-      return Promise.reject(e);
-    }
+  async httpPost<T = any>(url: string, data: any): Promise<T> {
+    const response = await this.bitcoinApi.post(url, data);
+    return response.data;
   }
 }
