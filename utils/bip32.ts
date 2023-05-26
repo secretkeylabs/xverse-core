19c2ec3a84b72ef36d3e6cdc6b72288c0ab236d6
Victor <victor@nomanini.com>
2023-05-26 13:21:56 +0300

    fix: switch to @bitcoinerlab/secp256k1 The tiny-secp256k1 package is wasm based and not supported by react native for android. @noble/secp256k1 was also installed and tested but it does not have the correct interface for bip32 and ECPair. @bitcoinerlab/secp256k1 was tested successfully and was chosen.

--- utils/bip32.ts
+++ utils/bip32.ts
@@ -1,5 +1,5 @@
+import * as ecc from '@bitcoinerlab/secp256k1';
 import { BIP32Factory, BIP32Interface } from 'bip32';
-import * as ecc from 'tiny-secp256k1';
 
 export const bip32 = BIP32Factory(ecc);
 export type { BIP32Interface };
--- utils/ecpair.ts
+++ utils/ecpair.ts
@@ -1,5 +1,5 @@
+import * as ecc from '@bitcoinerlab/secp256k1';
 import { ECPairFactory, ECPairInterface } from 'ecpair';
-import * as ecc from 'tiny-secp256k1';
 
 export const ECPair = ECPairFactory(ecc);
 export type { ECPairInterface };
