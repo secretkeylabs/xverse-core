119d0c5175fea2fa6ae63eb65d51db36921674af
victorkirov <victor.kirov@gmail.com>
2023-07-28 16:59:58 +0300

    feat: Add all brc-20 api calls

--- api/xverseInscribe.ts
+++ api/xverseInscribe.ts
@@ -52,11 +52,95 @@ const createBrc20TransferOrder = async (
     network,
     inscriptionValue,
   };
-  const response = await apiClient.post<Brc20CreateOrderResponse>('/v1/brc20/cost-estimate', requestBody);
+  const response = await apiClient.post<Brc20CreateOrderResponse>('/v1/brc20/place-order', requestBody);
   return response.data;
 };
 
-const executeBrc20TransferOrder = async (
+const getBrc20MintFees = async (
+  tick: string,
+  amount: number,
+  revealAddress: string,
+  feeRate: number,
+  inscriptionValue?: number,
+): Promise<Brc20CostEstimateResponse> => {
+  const requestBody: Brc20CostEstimateRequest = {
+    operation: 'mint',
+    tick,
+    amount,
+    revealAddress,
+    feeRate,
+    inscriptionValue,
+  };
+  const response = await apiClient.post<Brc20CostEstimateResponse>('/v1/brc20/cost-estimate', requestBody);
+  return response.data;
+};
+
+const createBrc20MintOrder = async (
+  tick: string,
+  amount: number,
+  revealAddress: string,
+  feeRate: number,
+  network: NetworkType,
+  inscriptionValue?: number,
+): Promise<Brc20CreateOrderResponse> => {
+  const requestBody: Brc20CreateOrderRequest = {
+    operation: 'mint',
+    tick,
+    amount,
+    revealAddress,
+    feeRate,
+    network,
+    inscriptionValue,
+  };
+  const response = await apiClient.post<Brc20CreateOrderResponse>('/v1/brc20/place-order', requestBody);
+  return response.data;
+};
+
+const getBrc20DeployFees = async (
+  tick: string,
+  max: number,
+  limit: number,
+  revealAddress: string,
+  feeRate: number,
+  inscriptionValue?: number,
+): Promise<Brc20CostEstimateResponse> => {
+  const requestBody: Brc20CostEstimateRequest = {
+    operation: 'deploy',
+    tick,
+    lim: limit,
+    max: max,
+    revealAddress,
+    feeRate,
+    inscriptionValue,
+  };
+  const response = await apiClient.post<Brc20CostEstimateResponse>('/v1/brc20/cost-estimate', requestBody);
+  return response.data;
+};
+
+const createBrc20DeployOrder = async (
+  tick: string,
+  max: number,
+  limit: number,
+  revealAddress: string,
+  feeRate: number,
+  network: NetworkType,
+  inscriptionValue?: number,
+): Promise<Brc20CreateOrderResponse> => {
+  const requestBody: Brc20CreateOrderRequest = {
+    operation: 'deploy',
+    tick,
+    lim: limit,
+    max: max,
+    revealAddress,
+    feeRate,
+    network,
+    inscriptionValue,
+  };
+  const response = await apiClient.post<Brc20CreateOrderResponse>('/v1/brc20/place-order', requestBody);
+  return response.data;
+};
+
+const executeBrc20Order = async (
   commitAddress: string,
   commitTransactionHex: string,
 ): Promise<Brc20ExecuteOrderResponse> => {
@@ -64,12 +148,16 @@ const executeBrc20TransferOrder = async (
     commitAddress,
     commitTransactionHex,
   };
-  const response = await apiClient.post<Brc20ExecuteOrderResponse>('/v1/brc20/cost-estimate', requestBody);
+  const response = await apiClient.post<Brc20ExecuteOrderResponse>('/v1/brc20/execute-order', requestBody);
   return response.data;
 };
 
 export default {
   getBrc20TransferFees,
   createBrc20TransferOrder,
-  executeBrc20TransferOrder,
+  getBrc20MintFees,
+  createBrc20MintOrder,
+  getBrc20DeployFees,
+  createBrc20DeployOrder,
+  executeBrc20Order,
 };
