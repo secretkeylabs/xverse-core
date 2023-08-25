bbfc6ffcacdf7d004bfa42c36bb7ce448652ae91
Abdul Haseeb <haseeb4239@gmail.com>
2023-08-25 22:12:23 +0500

    fix: tests

--- transactions/btc.ts
+++ transactions/btc.ts
@@ -8,7 +8,7 @@ import BitcoinEsploraApiProvider from '../api/esplora/esploraAPiProvider';
 import { fetchBtcFeeRate } from '../api/xverse';
 import { BtcFeeResponse, ErrorCodes, Inscription, NetworkType, ResponseError, UTXO } from '../types';
 import { getBtcPrivateKey, getBtcTaprootPrivateKey } from '../wallet';
-import { selectOptimalUtxos } from './btc.utils';
+import { getOrdinalsUtxos, selectOptimalUtxos } from './btc.utils';
 import { BitcoinNetwork, getBtcNetwork } from './btcNetwork';
 
 const MINIMUM_CHANGE_OUTPUT_SATS = 1000;
@@ -501,12 +501,21 @@ export async function getBtcFeesForOrdinalTransaction(feeParams: {
   const address = isRecover ? btcAddress : ordinalsAddress;
   const addressUtxos = await btcClient.getUnspentUtxos(address);
   const ordUtxo = getOrdinalUtxo(addressUtxos, ordinal);
+  const addressOrdinalsUtxos = await getOrdinalsUtxos(btcAddress);
   if (!ordUtxo) {
     // TODO: Throw error and not just the code
     // eslint-disable-next-line @typescript-eslint/no-throw-literal
     throw new ResponseError(ErrorCodes.OrdinalUtxoNotfound).statusCode;
   }
-  return getBtcFeesForOrdinalSend(recipientAddress, ordUtxo, btcAddress, network, addressUtxos, feeMode, feeRateInput);
+  return getBtcFeesForOrdinalSend(
+    recipientAddress,
+    ordUtxo,
+    btcAddress,
+    network,
+    addressOrdinalsUtxos,
+    feeMode,
+    feeRateInput,
+  );
 }
 
 // Used to calculate fees for setting low/high fee settings
@@ -779,6 +788,7 @@ export async function signOrdinalSendTransaction(
   const ordinalUtxoInPaymentAddress = filteredUnspentOutputs.length < unspentOutputs.length;
 
   let feeRate: BtcFeeResponse = defaultFeeRate;
+
   let feePerVByte: BigNumber = new BigNumber(0);
 
   if (!fee) {
@@ -901,6 +911,7 @@ export async function signOrdinalTransaction(ordinalTxParams: {
   const address = isRecover ? btcAddress : ordinalsAddress;
   const addressUtxos = await btcClient.getUnspentUtxos(address);
   const ordUtxo = getOrdinalUtxo(addressUtxos, ordinal);
+  const addressOrdinalsUtxos = await getOrdinalsUtxos(btcAddress);
   if (!ordUtxo) {
     // TODO: Throw error and not just the code
     // eslint-disable-next-line @typescript-eslint/no-throw-literal
@@ -913,7 +924,7 @@ export async function signOrdinalTransaction(ordinalTxParams: {
     accountIndex,
     seedPhrase,
     network,
-    addressUtxos,
+    addressOrdinalsUtxos,
     fee,
   );
 }
