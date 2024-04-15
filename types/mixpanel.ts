5e6e97c3ce75960a92b27358dda872fc0cbd41b4
Den <36603049+dhriaznov@users.noreply.github.com>
2024-04-15 11:28:45 +0200

    Add new values to the AnalyticsEvents enum (#27)
    * Add new values to the AnalyticsEvents enum
    
    * Update the AnalyticsEvents enum
    
    * Update the AnalyticsEvents enum
    
    * Add the PageViewed event to AnalyticsEvents enum
    
    * Add the TransactionConfirmedProperties and AnalyticsEventProperties types
    
    * simplify it
    
    * Add rare sats
    
    * extend the AnalyticsEventProperties type
    
    * extend the AnalyticsEventProperties type
    
    * simplify it
    
    * Add the ClickApp event
    
    * Use the AccountType type
    
    * Trigger build
    
    * Trigger build
    
    ---------
    
    Co-authored-by: Tim Man <tim@secretkeylabs.com>

--- types/mixpanel.ts
+++ types/mixpanel.ts
@@ -1,5 +1,45 @@
+import { AccountType } from './account';
+
 export enum AnalyticsEvents {
   OptOut = 'Opt Out',
   CreateNewWallet = 'Create new wallet',
   RestoreWallet = 'Restore wallet',
+  ClickApp = 'click_app',
+  PageViewed = 'page_viewed',
+  AppConnected = 'app_connected',
+  TransactionConfirmed = 'transaction_confirmed',
+  PSBTSigningRequestReceived = 'psbt_signing_request_received',
+  MessageSigningRequestReceived = 'message_signing_request_received',
+  TransactionSigningRequestReceived = 'transaction_signing_request_received',
 }
+
+type CommonProps = {
+  wallet_type: AccountType;
+};
+
+export type AnalyticsEventProperties = {
+  [AnalyticsEvents.PageViewed]: {
+    path: string;
+    tab?: string;
+  } & CommonProps;
+  [AnalyticsEvents.AppConnected]: {
+    requestedAddress: string[];
+  } & CommonProps;
+  [AnalyticsEvents.TransactionConfirmed]: {
+    protocol: 'brc20' | 'sip10' | 'bitcoin' | 'stacks' | 'runes' | 'ordinals' | 'rare-sats' | 'stacks-nfts';
+    action: 'inscribe' | 'transfer' | 'sign-message' | 'sign-psbt' | 'sign-batch-psbt';
+    repeat?: true;
+    batch?: true;
+  } & CommonProps;
+  [AnalyticsEvents.PSBTSigningRequestReceived]: {
+    batch?: true;
+  } & CommonProps;
+  [AnalyticsEvents.MessageSigningRequestReceived]: {
+    protocol: 'bitcoin' | 'stacks';
+    structured?: true;
+  } & CommonProps;
+  [AnalyticsEvents.TransactionSigningRequestReceived]: {
+    protocol: 'stacks';
+    action: 'token_transfer' | 'contract_call' | 'smart_contract' | 'transfer';
+  } & CommonProps;
+};
