98d51d8cc4f28a0fab543fea64fd568f35e4edaf
Den <36603049+dhriaznov@users.noreply.github.com>
2024-04-24 08:23:28 +0200

    Update the AnalyticsEvents enum and related type (#50)
    * Update the AnalyticsEvents enum and related type
    
    * Update the TransactionConfirmed event type

--- types/mixpanel.ts
+++ types/mixpanel.ts
@@ -5,12 +5,8 @@ export enum AnalyticsEvents {
   CreateNewWallet = 'Create new wallet',
   RestoreWallet = 'Restore wallet',
   ClickApp = 'click_app',
-  PageViewed = 'page_viewed',
   AppConnected = 'app_connected',
   TransactionConfirmed = 'transaction_confirmed',
-  PSBTSigningRequestReceived = 'psbt_signing_request_received',
-  MessageSigningRequestReceived = 'message_signing_request_received',
-  TransactionSigningRequestReceived = 'transaction_signing_request_received',
 }
 
 type CommonProps = {
@@ -18,28 +14,19 @@ type CommonProps = {
 };
 
 export type AnalyticsEventProperties = {
-  [AnalyticsEvents.PageViewed]: {
-    path: string;
-    tab?: string;
-  } & CommonProps;
+  [AnalyticsEvents.ClickApp]: {
+    link: string;
+    source: string;
+    title?: string;
+    section?: string;
+  };
   [AnalyticsEvents.AppConnected]: {
     requestedAddress: string[];
   } & CommonProps;
   [AnalyticsEvents.TransactionConfirmed]: {
     protocol: 'brc20' | 'sip10' | 'bitcoin' | 'stacks' | 'runes' | 'ordinals' | 'rare-sats' | 'stacks-nfts';
     action: 'inscribe' | 'transfer' | 'sign-message' | 'sign-psbt' | 'sign-batch-psbt';
-    repeat?: true;
-    batch?: true;
-  } & CommonProps;
-  [AnalyticsEvents.PSBTSigningRequestReceived]: {
-    batch?: true;
-  } & CommonProps;
-  [AnalyticsEvents.MessageSigningRequestReceived]: {
-    protocol: 'bitcoin' | 'stacks';
-    structured?: true;
-  } & CommonProps;
-  [AnalyticsEvents.TransactionSigningRequestReceived]: {
-    protocol: 'stacks';
-    action: 'token_transfer' | 'contract_call' | 'smart_contract' | 'transfer';
+    repeat?: number;
+    batch?: number;
   } & CommonProps;
 };
