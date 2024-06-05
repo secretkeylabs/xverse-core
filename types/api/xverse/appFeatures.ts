4d67d694d5cedf8404823138d58c8dd6d6e2acd1
Den <36603049+dhriaznov@users.noreply.github.com>
2024-06-05 14:00:07 +0200

    [ENG-4430] Add SWAPS as a feature flag option (#95)

--- types/api/xverse/appFeatures.ts
+++ types/api/xverse/appFeatures.ts
@@ -1,6 +1,6 @@
 import { NetworkType } from '../../network';
 
-export type FeatureId = 'RUNES_SUPPORT';
+export type FeatureId = 'RUNES_SUPPORT' | 'SWAPS';
 
 export type AppFeaturesContext = {
   network: NetworkType;
