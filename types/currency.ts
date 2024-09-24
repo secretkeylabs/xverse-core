7a8a0a2ab84e95d912894878897d524bc746aa57
Terence Ng <ngtere@live.com.sg>
2024-09-24 18:57:21 +0800

    add new fiat currencies (#196)
    * add new currencies
    
    * remove unsupported currencies
    
    * Update currency.ts
    
    * fix up list
    
    * Update currency.ts

--- types/currency.ts
+++ types/currency.ts
@@ -12,10 +12,10 @@ export type BtcUsdCurrencyRate = {
 };
 
 export type SupportedCurrency =
-  | 'USD'
-  | 'EUR'
   | 'CAD'
   | 'CNY'
+  | 'EUR'
+  | 'USD'
   | 'ARS'
   | 'KRW'
   | 'HKD'
@@ -24,8 +24,22 @@ export type SupportedCurrency =
   | 'GBP'
   | 'BRL'
   | 'RUB'
+  | 'AUD'
+  | 'NGN'
+  | 'TRY'
+  | 'INR'
   | 'CHF'
-  | 'AUD';
+  | 'VND'
+  | 'PLN'
+  | 'MYR'
+  | 'TWD'
+  | 'IDR'
+  | 'HUF'
+  | 'THB'
+  | 'PHP'
+  | 'PKR'
+  | 'ZAR'
+  | 'MXN';
 
 export const currencySymbolMap: Record<SupportedCurrency, string> = {
   USD: '$',
@@ -34,12 +48,26 @@ export const currencySymbolMap: Record<SupportedCurrency, string> = {
   CNY: '¥',
   ARS: '$',
   KRW: '₩',
-  HKD: 'HK$',
+  HKD: '$',
   JPY: '¥',
-  SGD: 'S$',
+  SGD: '$',
   GBP: '£',
   BRL: 'R$',
   RUB: '₽',
-  CHF: '₣',
-  AUD: 'AU$',
+  AUD: '$',
+  NGN: '₦', // Nigerian Naira
+  TRY: 'TL', // Turkish Lira
+  INR: '₨', // Indian Rupee
+  CHF: 'CHF', // Swiss Franc
+  VND: '₫', // Vietnamese Dong
+  PLN: 'zł', // Polish Zloty
+  MYR: 'RM', // Malaysian Ringgit
+  TWD: 'NT$', // New Taiwan Dollar
+  IDR: 'Rp', // Indonesian Rupiah
+  HUF: 'Ft', // Hungarian Forint
+  THB: '฿', // Thai Baht
+  PHP: '₱', // Philippine Peso
+  PKR: '₨', // Pakistani Rupee
+  ZAR: 'R', // South African Rand
+  MXN: '$', // Mexican Peso
 };
