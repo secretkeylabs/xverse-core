0c8272cf2664b90619a3fcbe0e3b3d6901b670b7
Christos Maris <christos@secretkeylabs.com>
2024-12-12 14:55:29 +0200

    [ENG-5224] Implement Portfolio Tracking V2

--- types/account.ts
+++ types/account.ts
@@ -63,3 +63,7 @@ export type ExchangeRateList = {
 };
 
 export type ExchangeRateAvailableCurrencies = 'USD' | 'BTC' | 'STX';
+
+export const HistoricalDataPeriods = ['1d', '1w', '1m', '1y'] as const;
+export type HistoricalDataParamsPeriod = (typeof HistoricalDataPeriods)[number];
+export type HistoricalDataResponsePrices = { x: number; y: number; tooltipLabel: string }[];
