3b59b1fd17c748b954dd2c581bbfa5930c9b4319
jordankzf <jordan@secretkeylabs.com>
2025-02-25 17:10:20 +0800

    Remove nonce type

--- types/api/xverse/wallet.ts
+++ types/api/xverse/wallet.ts
@@ -14,7 +14,6 @@ export type StxAddressData = {
   balance: BigNumber;
   availableBalance: BigNumber;
   locked: BigNumber;
-  nonce: number;
   transactions: Array<TransactionData>;
   totalTransactions?: number;
 };
